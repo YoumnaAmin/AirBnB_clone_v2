0de2418a28549816624d775da6f4c63d4f4928da
YoumnaAmin <youmnamin564@gmail.com>
2024-03-21 04:26:23 +0300

    youmna

--- models/city.py
+++ models/city.py
@@ -5,10 +5,12 @@ from sqlalchemy import Column, Integer, String, create_engine, ForeignKey
 from sqlalchemy import CHAR, DateTime,func
 from sqlalchemy.ext.declarative import declarative_base
 from models.state import State
-
+from sqlalchemy.orm import relationship, backref
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
     state_id = Column(String,ForeignKey(State.id), nullable=False)
     name = Column(String(128), nullable=False)
+    places = relationship("Place", backref="user",\
+                          cascade="all, delete, delete-orphan")
