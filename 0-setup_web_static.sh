860653d65fd109ed4ad67098848fa310e3c7c46e
YoumnaAmin <youmnamin564@gmail.com>
2024-05-20 00:52:31 +0300

    ..

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+
+# Function to install Nginx
+install_nginx() {
+    if ! nginx -v &> /dev/null; then
+        sudo apt update
+        sudo apt install -y nginx
+    fi
+}
+
+# Function to create a directory if it doesn't exist
+create_directory() {
+    local dir=$1
+    if [ ! -d "$dir" ]; then
+        sudo mkdir -p "$dir"
+    fi
+}
+
+# Function to create a fake HTML file
+create_fake_html() {
+    locla file="$1"
+    echo "<!DOCTYPE html>
+    <html>
+    <head>
+        <title>TEST</title>
+    </head>
+    <body>
+        <h1>Test</h1>
+    </body>
+    </html>" > "$(file)"
+}
+
+
+# Function to create a symbolic link
+craete_symbolic_link () {
+    local target=$1
+    local link=$2
+    if [ "$link" ];
+    then
+        sudo rm -f "link"
+    fi
+    sudo ln -s "$target" "$link"
+}
+
+# Function to set ownership
+set_ownership() {
+    local dir=$1
+    sudo chown -R ubuntu:ubuntu "$dir"
+}
+
+
+# Function to update Nginx configuration
+update_nginx_configuration() {
+    local nginx_conf="/etc/nginx/sites-available/default"
+    sudo sed -i '/server_name _;/a
+    location /hbnb_static {\\n
+        alias /data/web_static/current/;\\n
+        autoindex off;\\n
+    }' "$nginx_conf"
+    sudo systemctl restart nginx
+}
+
+# Run the functions
+install_nginx
+create_directory "/data/"
+create_directory "/data/web_static/"
+create_directory "/data/web_static/releases/"
+create_directory "/data/web_static/shared/"
+create_directory "/data/web_static/releases/test/"
+create_fake_html "/data/web_static/releases/test/index.html"
+create_symbolic_link "/data/web_static/releases/test/" "/data/web_static/current"
+set_ownership "/data/"
+update_nginx_configuration
