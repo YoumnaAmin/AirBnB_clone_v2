761c5491798e1f15cf2a60ba31e95ebb6079870e
YoumnaAmin <youmnamin564@gmail.com>
2024-06-02 01:05:12 +0300

    ..

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """home page"""
-    return "<h1>Hello HBNB!</h1>"
+    return "Hello HBNB!"
 
 
 if __name__ == '__main__':
