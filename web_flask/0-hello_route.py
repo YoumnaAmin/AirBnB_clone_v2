545d7ab1343912e2344356f19f2a2027d8e76669
YoumnaAmin <youmnamin564@gmail.com>
2024-06-02 01:09:22 +0300

    ..

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-"""0-hello module"""
+"""0-hello module
 
+This module starts a Flask web application that returns "Hello HBNB!"
+when accessing the root URL.
+"""
 
 from flask import Flask
 
-
 app = Flask(__name__)
+
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """home page"""
+    """Home page route that returns 'Hello HBNB!'"""
     return "Hello HBNB!"
 
 
