6237403f6645a2b929f421e341baa9e0fa205736
YoumnaAmin <youmnamin564@gmail.com>
2024-06-02 00:47:33 +0300

    ..

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""0-hello module"""
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+@app.route("/")
+def hello_hbnb():
+    """home page"""
+    return "<h1>Hello HBNB!</h1>"
+
+if __name__ == '__main__':
+    app.run()
