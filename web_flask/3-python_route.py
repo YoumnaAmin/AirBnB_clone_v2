c85f8d2236b24b626a8ecd95b4826c944b6841df
YoumnaAmin <youmnamin564@gmail.com>
2024-06-02 03:14:17 +0300

    ..

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""0-hello module
+
+This module starts a Flask web application that returns "Hello HBNB!"
+when accessing the root URL.
+"""
+
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Home page route that returns 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def slash_hbnb():
+    """display HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """display c followed by text"""
+    return f"C {escape(text.replace('_', ' '))}"
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text='is cool'):
+    """display c followed by text"""
+    return f"Python {escape(text.replace('_', ' '))}"
+
+
+if __name__ == '__main__':
+    app.run(debug=False)
