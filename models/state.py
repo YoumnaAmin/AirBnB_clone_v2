3ba55efe34de61733095be5287f4e38dffb6b560
YoumnaAmin <youmnamin564@gmail.com>
2024-06-03 03:04:36 +0300

    ..

--- models/state.py
+++ models/state.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""State module for HBNB project"""
 
-from AirBnB_clone_v2.models.city import City
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 import models
 
 
-class State(BaseModel):
-    """ State class """
+class State(BaseModel, Base):
+    """State class"""
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
 
-     # Relationship for DBStorage
-    cities = relationship("City", backref="State", cascade="all, delete-orphan")
+    # Relationship for DBStorage
+    cities = relationship("City", backref="state", cascade="all, delete, delete-orphan")
 
     @property
     def cities(self):
         """Return the list of City objects from storage linked to the current State"""
         if models.storage_t != 'db':
+            from models.city import City  # Delayed import to avoid circular dependency
             return [city for city in models.storage.all(City).values() if city.state_id == self.id]
